3c1524e855e1f1a84f9ee6db7ea6d73016bac795
unknown <IT@AtlantisPC-025.atlantisbpo.local>
2021-12-21 12:57:13 +0500

    update user.php and add session.php

--- include/user.php
+++ include/user.php
@@ -2,26 +2,77 @@
 require_once('database.php');
 
 class User{
+    public $id;
+    public $username;
+    public $password;
+    public $first_name;
+    public $last_name;
 
     public static function find_all(){
-        global $database;
-        $result_set=$database->query("SELECT * FROM users");
-        return $result_set;
+        // global $database;
+        // $result_set=$database->query("SELECT * FROM users");        
+        // return $result_set;
+
+        return self::find_by_sql('SELECT * FROM users');
     }
 
-    public static function find_by_id($id){
-        global $database;
-        $result_set =$database->query("SELECT * FROM users WHERE id={$id}");
-        $found=$database->fetch_array($result_set);
-        return $found;
+    public static function find_by_id($id=0){
+        // global $database;
+        // $result_set =$database->query("SELECT * FROM users WHERE id={$id}");
+        // $found=$database->fetch_array($result_set);     
+        // return $found;
+        $result_array=self::find_by_sql("SELECT * FROM users WHERE id={$id} LIMIT 1");
+        return !empty($result_array) ? array_shift($result_array) : false ;
+
+       
     }
 
     public static function find_by_sql($sql=""){
         global $database;
         $result_set =$database->query($sql);
 
-        return $result_set;
+        $object_array=array();
+        while($row=$database->fetch_array($result_set)){
+            $object_array[]=self::instantiate($row);
+        }
+
+        return $object_array;
     }
+
+    public function full_name(){
+        if(isset($this->first_name) && isset($this->last_name)){
+            return $this->first_name . " " . $this->last_name;
+        }else{
+            return "";
+        }
+    }
+
+    private static function instantiate($record){
+        // $object =new User();
+        // $object->id=$record['id'];
+        // $object->usename=$record['objectname'];
+        // $object->password=$record['password'];
+        // $object->first_name=$record['first_name'];
+        // $object->last_name=$record['last_name'];
+        
+        // More dynamic, short-form approach
+        $object =new self();        
+        foreach($record as $attribute=>$value){
+            if($object->has_attribule($attribute)){
+                $object->$attribute=$value;             
+
+            }
+        }
+
+        return $object;
+    }
+
+    private function has_attribule($attribute){
+        $object_vars =get_object_vars($this);
+        return array_key_exists($attribute,$object_vars);
+    }
+
+
 
 
 
--- public/index.php
+++ public/index.php
@@ -2,41 +2,19 @@
     require_once('../include/database.php');
     require_once('../include/user.php');
 
-    if(isset($database)){echo "true";}else{echo "false";}
-    echo "<br/>";
 
 
+    // $user =User::find_by_id(1);
+    // echo $user->full_name();
 
+    $users=User::find_all();
 
-//     $sql="INSERT INTO users(username,password,first_name,last_name) VALUES ('mubassir11','secret','Mubassir','Ali')";
-
-//    $result =$database->query($sql);
-
-    $sql1="SELECT * FROM users WHERE id=1";
-    $result_set =$database->query($sql1);
-
-    $found_user=$database->fetch_array($result_set);
-
-    echo $found_user['username'];
-
-    echo "<hr/>";
-
-    // $user=new User();
-    // $found_user=$user->find_by_id(1);
-    // echo $found_user['first_name'];
-
-    $found_user =User::find_by_id(1);
-    echo $found_user['username'];
-
-    echo "<hr/>";
-
-    $user_set=User::find_all();
-    while($user=$database->fetch_array($user_set)){
-        echo "User: ".$user['username']."<br/>";
-        echo "first Name: ".$user['first_name']."<br/>";
-        echo "last Name: ".$user['last_name']."<br/>";
-        
+    foreach($users as $user){
+        echo "User: ". $user->username ."<br>";
+        echo "Name: ". $user->full_name(). "<br>";
     }
+
+
 
 
 ?>
